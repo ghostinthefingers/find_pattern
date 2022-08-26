65258b8a6f1ece64b345afc5e1c968db4748f438
GHOS7 1N 7H3 F1NG3RS <83473054+ghostinthefingers@users.noreply.github.com>
2022-08-27 07:52:15 +1000

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,16 @@
+from pwn import *
+import sys
+import logging
+
+p = cyclic(20000)
+if len(sys.argv) < 2:
+    logging.error('usage: python3 pattern.py 0xdeadbeef')
+    exit()
+
+address = sys.argv[1]
+if '0x' in address:
+    address = address[2:]
+    my_string = bytes.fromhex(address).decode('utf-8')
+    my_string = my_string[::-1]
+    print(f'padding is : {p.index(my_string.encode())}')
+    
