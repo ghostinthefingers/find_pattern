e2261e88efa9c718388bfee3e3b04bb55cff0274
V3in <93438059+V3innn@users.noreply.github.com>
2024-02-23 01:33:32 +0200

    Update pattern.py
    Just to be able to work efficient for both x32 bit architectures and x64 ! Nice work!

--- pattern.py
+++ pattern.py
@@ -2,15 +2,17 @@ from pwn import *
 import sys
 import logging
 
-p = cyclic(20000,n=8)
-if len(sys.argv) < 2:
-    logging.error('usage: python3 pattern.py 0xdeadbeef')
+if len(sys.argv) < 3:
+    logging.error('usage: python3 pattern.py <address> <n>')
     exit()
 
 address = sys.argv[1]
+n = int(sys.argv[2])
+
+p = cyclic(20000, n=n)
+
 if '0x' in address:
     address = address[2:]
     my_string = bytes.fromhex(address).decode('utf-8')
     my_string = my_string[::-1]
     print(f'padding is : {p.index(my_string.encode())}')
-    
